e4aafeb147be5c9fa1233fffc371e12bd4ea102d
NYLTI <al.firebird@mailfence.com>
2024-04-29 18:25:27 -0400

    initial UI using angular

--- cart/src/main/java/com/model/Cart.java
+++ cart/src/main/java/com/model/Cart.java
@@ -3,6 +3,7 @@ package com.model;
 import java.util.List;
 import java.util.Map;
 import org.bson.types.ObjectId;
+import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,6 +16,7 @@ public class Cart {
 	@Id
 	private ObjectId id;
 	@NotNull
+	@UniqueElements
 	private String userId;
 	private List<Map<ObjectId, Integer>> productList;
 }
